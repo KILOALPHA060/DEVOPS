13d14f71764e1a42d1b6841b07391a5aeb25c135
KILOALPHA060 <arjuna.kashyap32@gmail.com>
2025-06-20 15:33:31 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+echo "const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Cake Diaries Backend');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(\`Server running on port \${PORT}\`));
+" > server.js
